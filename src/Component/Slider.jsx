e8805582ec4a4965278b9a6b3ca5b8ab7738183d
Ajit raut <ajitraut9561@gmail.com>
2024-06-29 00:02:58 +0530

    Slider  Added

--- src/Component/Slider.jsx
+++ src/Component/Slider.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios';
 
 const Slider = () => {
+  const [newdata, setnewdata] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.145923&lng=79.08762999999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      )
+      .then((res) => {
+        console.log(res)
+        setnewdata(
+          res?.data?.data?.cards[0]?.card?.card?.gridElements?.infoWithStyle
+            ?.info
+        );
+        // setfiltersearch(
+        //   res?.data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        //     ?.restaurants
+        // );
+      });
+  }, []);
+
+
   return (
     <>
     <div className=' my-28'>
-      
+    <div>
+      {newdata.map((datt,index)=>
+      {
+      return (
+        <div>{datt.imageId}</div>
+      )}
+    )
+        
+      }
+    </div>
     </div>
     </>
   )
