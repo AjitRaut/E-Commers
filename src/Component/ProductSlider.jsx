1a0e68f407fb7612ea3efeeb1b3f5728346968b8
Ajit raut <ajitraut9561@gmail.com>
2024-07-01 22:44:06 +0530

    Some Chanegs

--- src/Component/ProductSlider.jsx
+++ src/Component/ProductSlider.jsx
@@ -1,40 +1,47 @@
-import React from 'react'
+import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { SliderIMG_URL } from "../Utils/Url";
 
 const ProductSlider = () => {
-    {
-        const [newdata, setnewdata] = useState([]);
-      
-        useEffect(() => {
-          axios
-            .get(
-              "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.145923&lng=79.08762999999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-            )
-            .then((res) => {
-              console.log(res);
-              setnewdata(
-                res?.data?.data?.cards[0]?.card?.card?.gridElements?.infoWithStyle
-                  ?.info
-              );
-            });
-        }, []);
-      
-    
-        return (
-          <>
-            <div className=" mt-28">
-              <div className=' flex '>
-                {newdata.map((datt, index) => {
-                  return <img className=" w-36 h-44" key={datt.imageId} src={`${SliderIMG_URL}${datt.imageId}`} />;
-                })}
-              
-              </div>
-            </div>
-          </>
-        );
-      };
-}
+  {
+    const [newdata, setnewdata] = useState([]);
 
-export default ProductSlider
+    useEffect(() => {
+      axios
+        .get(
+          "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.145923&lng=79.08762999999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        )
+        .then((res) => {
+          console.log(res);
+          setnewdata(
+            res?.data?.data?.cards[0]?.card?.card?.gridElements?.infoWithStyle
+              ?.info
+          );
+        });
+    }, []);
+
+    return (
+      <>
+        <div className=" mt-28 w-full h-auto">
+          <div className="max-w-6xl m-auto h-full overflow-hidden">
+          <div className=" flex ">
+            {newdata.map((datt, index) => {
+              return (
+                <img
+                  className=" w-36 h-46"
+                  key={datt.imageId}
+                  src={`${SliderIMG_URL}${datt.imageId}`}
+                />
+              );
+            })}
+          </div>
+          </div>
+         
+        </div>
+      </>
+    );
+  }
+};
+
+export default ProductSlider;
