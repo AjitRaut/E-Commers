aee9acbb13969986e559910d04d8baf238bb2e27
Ajit raut <ajitraut9561@gmail.com>
2024-07-01 18:19:57 +0530

    Some Changes

--- src/Component/Slider.jsx
+++ src/Component/Slider.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import axios from 'axios';
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { SliderIMG_URL } from "../Utils/Url";
 
 const Slider = () => {
   const [newdata, setnewdata] = useState([]);
@@ -11,36 +12,25 @@ const Slider = () => {
         "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.145923&lng=79.08762999999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
       )
       .then((res) => {
-        console.log(res)
+        console.log(res);
         setnewdata(
           res?.data?.data?.cards[0]?.card?.card?.gridElements?.infoWithStyle
             ?.info
         );
-        // setfiltersearch(
-        //   res?.data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
-        //     ?.restaurants
-        // );
       });
   }, []);
 
-
   return (
     <>
-    <div className=' my-28'>
-    <div>
-      {newdata.map((datt,index)=>
-      {
-      return (
-        <div>{datt.imageId}</div>
-      )}
-    )
-        
-      }
-    </div>
-    </div>
+      <div className=" my-28">
+        <div>
+          {newdata.map((datt, index) => {
+            return <img src="{datt.imageId}"></img>;
+          })}
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Slider
-
+export default Slider;
--- src/Utils/Url.jsx
+++ src/Utils/Url.jsx
@@ -1,4 +1,7 @@
 export const IMG_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-  export const LOGO = "../assets/logo.svg"; // Adjust the path if necessary
+export const LOGO = "../assets/logo.svg"; // Adjust the path if necessary
+
+export const SliderIMG_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/";
