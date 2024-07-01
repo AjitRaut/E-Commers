17845ca56802736ea0261810a021a3c088f3765d
Ajit raut <ajitraut9561@gmail.com>
2024-07-01 23:44:30 +0530

    Slider Styling added

--- src/Component/ProductSlider.jsx
+++ src/Component/ProductSlider.jsx
@@ -7,6 +7,16 @@ import { GoArrowLeft, GoArrowRight } from "react-icons/go";
 const ProductSlider = () => {
   {
     const [newdata, setnewdata] = useState([]);
+    const [Slideritem , SetSlideritem ] = useState(0)
+
+    const Prev=()=>{
+      if(newdata.length - 8 === Slideritem )return false;
+      SetSlideritem(Slideritem + 3)
+    }
+    const Next=()=>{
+      if(Slideritem === 0 )return false;
+      SetSlideritem(Slideritem - 3)
+    }
 
     useEffect(() => {
       axios
@@ -26,19 +36,28 @@ const ProductSlider = () => {
       <>
         <div className=" mt-28 w-full h-auto">
           <div className="max-w-6xl m-auto h-full overflow-hidden">
-            <div>
-              <GoArrowLeft className=" h-8 w-8 bg-slate-300 rounded-full p-2 inline" />
-
-              <GoArrowRight className=" h-8 w-8 bg-slate-300 rounded-full p-2 inline" />
+            <div className=" flex justify-between">
+              <div>What in your mind</div>
+              <div>
+                <button onClick={Prev}>
+                <GoArrowLeft  className=" h-8 w-8 bg-slate-300 rounded-full p-2 inline mr-2" />
+                </button>
+                <button onClick={Next}>
+                <GoArrowRight className=" h-8 w-8 bg-slate-300 rounded-full p-2 inline mr-2" />
+                </button>
+              </div>
             </div>
+
             <div className=" flex ">
-              {newdata.map((datt, index) => {
+              {newdata.map((datt) => {
                 return (
-                  <img
+                  
+                     <img style={{transform:`translateX(-${Slideritem * 100}%)`}}
                     className=" w-36 h-46"
                     key={datt.imageId}
                     src={`${SliderIMG_URL}${datt.imageId}`}
                   />
+                 
                 );
               })}
             </div>
