08dc0db981164da0003412cb1d156b7107628113
Ajit raut <ajitraut9561@gmail.com>
2024-07-11 17:53:29 +0530

    Styling Addin

--- src/Component/ProductsMenu.jsx
+++ src/Component/ProductsMenu.jsx
@@ -24,20 +24,20 @@ const ProductsMenu = () => {
       {pmenu.length === 0 ? (
         <ShimmerUi />
       ) : (
-        <div className="mt-20 w-full">
-          <div className=" max-w-6xl  m-auto">
-            <div className=" mx-32  font-bold">
-              <div className=" mt-40">
+        <div className="mt-12 w-full">
+          <div className=" max-w-3xl m-auto flex place-items-center">
+            <div className=" ml-14 font-bold w-full ">
+              <div className="p-3 mt-32">
                 <h1 className=" font-bold text-2xl">{name}</h1>
               </div>
-              <div className=" mt-7 p-5 box shadow-lg bg-gray-500">
+              <div className=" p-3 bg-white border-solid border-2 border-slate-200 rounded-xl">
                 <span>{avgRating}</span>
                 <span>({totalRatingsString})</span>
                 <span className=" m-2 font-serif text-center text-xl text-slate-400 leading-4">
                   .
                 </span>
                 <span>{costForTwoMessage}</span>
-                <div>{cuisines.join(" ,")}</div>
+                <div className=" text-orange-600 font-medium text-sm">{cuisines.join(" ,")}</div>
               </div>
             </div>
           </div>
