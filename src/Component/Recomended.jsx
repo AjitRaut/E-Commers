6b0aa8d8dc7bbc402ce3191b2a0c4aacedab5ca9
Ajit raut <ajitraut9561@gmail.com>
2024-07-14 22:32:26 +0530

    Some Changes

--- src/Component/Recomended.jsx
+++ src/Component/Recomended.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
 import useProductMenu from "../Utils/useProductMenu";
+import { Recomended_IMG_URL } from "../Utils/Url";
 
 const Recomended = () => {
   const { infoId } = useParams();
@@ -15,15 +16,27 @@ const Recomended = () => {
   return (
     <>
       <div>
+        <div className="px-3">
+            <h1 className="font-extrabold text-base">Recomended({recommended.length})</h1>
+        </div>
         <div>
           {recommended.map((recom, index) => (
-            <div key={index}>
-              <div> {recom.card.info.name}</div>
-              <div>{recom.card.info.defaultPrice/100}</div>
-              <span>{recom.card.info.ratings.aggregatedRating.rating}</span>
-              {/* <span>({recom.card.info.ratings.aggregatedRating.ratingCountV2
+            <div className=" flex justify-between p-3">
+              <div key={index}>
+                <div> {recom.card.info.name}</div>
+                <div>{recom.card.info.defaultPrice / 100}</div>
+                <span>{recom.card.info.ratings.aggregatedRating.rating}</span>
+                {/* <span>({recom.card.info.ratings.aggregatedRating.ratingCountV2
               })</span> */}
-              <div>{recom.card.info.description}</div>
+                <div>{recom.card.info.description}</div>
+              </div>
+              <div>
+                <img
+                  className="h-36 w-36 rounded-lg object-cover"
+                  src={Recomended_IMG_URL + recom.card.info.imageId}
+                  alt=""
+                />
+              </div>
             </div>
           ))}
         </div>
--- src/Utils/Url.jsx
+++ src/Utils/Url.jsx
@@ -6,5 +6,8 @@ export const LOGO = "../assets/logo.svg"; // Adjust the path if necessary
 export const SliderIMG_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/";
 
+export const Recomended_IMG_URL=
+"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/"
+
 export const ProductsMenu_URL =
   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1458004&lng=79.0881546&restaurantId=";
