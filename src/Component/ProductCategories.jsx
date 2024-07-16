c13912557142ee243e19a9340d18cb42eb61ed3a
Ajit raut <ajitraut9561@gmail.com>
2024-07-16 12:18:25 +0530

    Some Changes

--- src/Component/ProductCategories.jsx
+++ src/Component/ProductCategories.jsx
@@ -6,49 +6,43 @@ import { MdKeyboardArrowDown } from "react-icons/md";
 import ProductItemsCrad from "./ProductItemsCrad";
 import ProductNestedCategories from "./ProductNestedCategories";
 
-const ProductCategories = ({categories}) => {
-    console.log(categories)
+const ProductCategories = ({ categories }) => {
+  console.log(categories);
   const { infoId } = useParams();
   const pmenu = useProductMenu(infoId);
 
-  const [show , setshow] = useState(false)
-  //   console.log(pmenu?.cards?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards);
-
-//   const cat =
-//     pmenu?.cards?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
-//       (c) =>
-//         c.card?.card?.["@type"] ===
-//           "type.googleapis.com/swiggy.presentation.food.v2.NestedItemCategory" ||
-//         c.card?.card?.["@type"] ===
-//           "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
-//     );
-//   console.log(cat);
-const handleshow =()=>{
-    setshow(!show)
-}
+  const [show, setshow] = useState(false);
+  
+  const handleshow = () => {
+    setshow(!show);
+  };
 
   return (
     <>
       <div>
-       
-          <div key={categories.title}>
-            <div className="flex justify-between place-items-center border-t-[12px] bg-white cursor-pointer" onClick={handleshow}>
-              <div className="text-base font-bold">
+        <div key={categories.title}>
+          <div
+            className="flex justify-between place-items-center border-t-[12px] bg-white cursor-pointer"
+            onClick={handleshow}
+          >
+            <div className="text-lg font-bold p-[10px]">
               {categories.title}
               {categories.itemCards && ` (${categories.itemCards.length})`}
-              </div>
-              <div>
-                <MdKeyboardArrowDown className="h-6 w-6" />
-              </div>
             </div>
-       {show && 
-            <ProductItemsCrad itemCards={categories.itemCards} nestedCategories={categories.categories}  />
-        }
-        {show && 
-            <ProductNestedCategories nestedCategories={categories.categories} />
-        }
+            <div>
+              <MdKeyboardArrowDown className="h-6 w-6" />
+            </div>
           </div>
-      
+          {show && (
+            <ProductItemsCrad
+              itemCards={categories.itemCards}
+              nestedCategories={categories.categories}
+            />
+          )}
+          {show && (
+            <ProductNestedCategories nestedCategories={categories.categories} />
+          )}
+        </div>
       </div>
     </>
   );
