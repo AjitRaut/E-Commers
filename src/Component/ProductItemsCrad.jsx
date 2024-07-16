7d5592679cf383d24fb1332454c9fce4658d0c8a
Ajit raut <ajitraut9561@gmail.com>
2024-07-16 12:31:19 +0530

    Some Changes

--- src/Component/ProductItemsCrad.jsx
+++ src/Component/ProductItemsCrad.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Recomended_IMG_URL } from "../Utils/Url";
 
-const ProductItemsCrad = ({ itemCards ,nestedCategories}) => {
+const ProductItemsCrad = ({ itemCards, nestedCategories }) => {
   console.log(itemCards);
-  console.log(nestedCategories)
+  console.log(nestedCategories);
 
   if (!itemCards) {
     return null;
@@ -13,28 +13,31 @@ const ProductItemsCrad = ({ itemCards ,nestedCategories}) => {
     <>
       <div>
         {itemCards.map((item) => (
-          <div className="flex justify-between p-3" key={item.card.info.id}>
-            <div>
-              <div className="font-bold text-lg text-slate-700">
-                {item.card.info.name}
+          <div>
+            <div className="flex justify-between p-3" key={item.card.info.id}>
+              <div>
+                <div className="font-bold text-lg text-slate-700">
+                  {item.card.info.name}
+                </div>
+                <div className="font-bold text-base">
+                  <span>&#8377;</span>
+                  {item.card.info.defaultPrice / 100 ||
+                    item.card.info.finalPrice / 100 ||
+                    item.card.info.price / 100}
+                </div>
+                <div className="font-extralight text-base text-slate-600">
+                  {item.card.info.description}
+                </div>
               </div>
-              <div className="font-bold text-base">
-                <span>&#8377;</span>
-                {item.card.info.defaultPrice / 100 ||
-                  item.card.info.finalPrice / 100 ||
-                  item.card.info.price / 100}
-              </div>
-              <div className="font-extralight text-base text-slate-600">
-                {item.card.info.description}
+              <div className="ml-5 w-3/12">
+                <img
+                  className="h-36 w-40 rounded-lg object-cover"
+                  src={Recomended_IMG_URL + item.card.info.imageId}
+                  alt=""
+                />
               </div>
             </div>
-            <div className="ml-5 w-3/12">
-              <img
-                className="h-36 w-36 rounded-lg object-cover"
-                src={Recomended_IMG_URL + item.card.info.imageId}
-                alt=""
-              />
-            </div>
+            <hr className="mb-3 border-t-2 border-gray-300" />
           </div>
         ))}
       </div>
