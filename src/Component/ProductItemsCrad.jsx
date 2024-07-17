9c0978a653314e5eec2dc4e9d0e82b4d6291755c
Ajit raut <ajitraut9561@gmail.com>
2024-07-17 09:40:18 +0530

    Some Changes

--- src/Component/ProductItemsCrad.jsx
+++ src/Component/ProductItemsCrad.jsx
@@ -14,7 +14,7 @@ const ProductItemsCrad = ({ itemCards, nestedCategories }) => {
       <div>
         {itemCards.map((item) => (
           <div className="w-full">
-            <div className="flex justify-between p-3" key={item.card.info.id}>
+            <div className="flex justify-between p-3 mb-6" key={item.card.info.id}>
               <div className="w-3/4">
                 <div className="font-bold text-lg text-slate-700">
                   {item.card.info.name}
@@ -29,14 +29,14 @@ const ProductItemsCrad = ({ itemCards, nestedCategories }) => {
                   {item.card.info.description}
                 </div>
               </div>
-              <div className="ml-8 w-3/12">
+              <div className="relative ml-8 w-3/12">
                 <img
                   className="h-36 w-full rounded-lg object-cover"
                   src={Recomended_IMG_URL + item.card.info.imageId}
                   alt=""
                 />
-                <div className="relative">
-                  <button className="bg-white h-8 w-24 border-2 border-gray-400">ADD</button>
+                <div className="absolute bottom-[-8px] w-full flex justify-center">
+                  <button className="bg-white h-8 w-24 border-2 shadow-md  rounded-lg px-5 text-center text-green-700 text-xl font-bold border-gray-400">ADD</button>
                 </div>
               </div>
             </div>
