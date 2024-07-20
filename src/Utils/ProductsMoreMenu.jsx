57928f74fa19b2ae233e86b27dc00d593200deec
Ajit raut <ajitraut9561@gmail.com>
2024-07-20 10:50:31 +0530

    Some Changes

--- src/Utils/ProductsMoreMenu.jsx
+++ src/Utils/ProductsMoreMenu.jsx
@@ -1,23 +1,18 @@
-// import React, { useEffect } from 'react'
-// import useProductMenueMore from './useProductMenueMore'
-// import axios from 'axios'
+import React, { useEffect } from "react";
+import useProductMenueMore from "./useProductMenueMore";
+import axios from "axios";
 
-// const ProductsMoreMenu = () => {
+const ProductsMoreMenu = () => {
+  useEffect(() => {
+    axios
+      .get(
+        "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1458004&lng=79.0881546&restaurantId=790758&catalog_qa=undefined&submitAction=ENTER"
+      )
+      .then((res) => {
+        console.log(res);
+      });
+  });
+  return <div></div>;
+};
 
-    
-//     useEffect(()=>{
-//         axios.get("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1458004&lng=79.0881546&restaurantId=790758&catalog_qa=undefined&submitAction=ENTER")
-//         .then((res)=>{
-//             console.log(res)
-//         })
-//     })
-//   return (
-//     <div>
-      
-
-//     </div>
-//   )
-// }
-
-// export default ProductsMoreMenu
-
+export default ProductsMoreMenu;
