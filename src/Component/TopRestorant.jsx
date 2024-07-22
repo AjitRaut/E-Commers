cf6806709d44e24226b96241fdc877da4f9bf54c
Ajit raut <ajitraut9561@gmail.com>
2024-07-22 22:39:00 +0530

    To Restorent modifying

--- src/Component/TopRestorant.jsx
+++ src/Component/TopRestorant.jsx
@@ -1,17 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { IMG_URL } from "../Utils/Url";
 import { IoMdStarOutline } from "react-icons/io";
+import { GoArrowLeft, GoArrowRight } from "react-icons/go";
 import UseMain from "../Utils/UseMain";
 import ShimmerUi from "./ShimmerUi";
 import { Link } from "react-router-dom";
-import { GoArrowLeft, GoArrowRight } from "react-icons/go";
-import { useState } from "react";
-
 
 const TopRestorant = () => {
   const [Slideritem, SetSlideritem] = useState(0);
   const newdata = UseMain();
-  
 
   if (
     !newdata ||
@@ -20,10 +17,16 @@ const TopRestorant = () => {
   ) {
     return <ShimmerUi />; 
   }
+
+  const restaurantInfo = newdata.cards[1].card.card.gridElements.infoWithStyle.restaurants;
+
+  if (!restaurantInfo || restaurantInfo.length === 0) {
+    return <ShimmerUi />; 
+  }
+
   const Next = () => {
     if (restaurantInfo.length - 11 <= Slideritem) return false;
     SetSlideritem(Slideritem + 1);
-    console.log(Slideritem)
   };
 
   const Prev = () => {
@@ -31,22 +34,11 @@ const TopRestorant = () => {
     SetSlideritem(Slideritem - 1);
   };
 
-  const restaurantInfo =
-    newdata.cards[1].card.card.gridElements.infoWithStyle.restaurants;
-
-  if (!restaurantInfo || restaurantInfo.length === 0) {
-    return <ShimmerUi />; 
-  }
-
   return (
     <>
-   
       <div className="max-w-6xl overflow-hidden mt-24 m-auto ">
-        
-      <div className="flex justify-between">
-          <h1 className="font-serif font-bold text-xl">
-          Top restaurant chains in Nagpur
-          </h1>
+        <div className="flex justify-between">
+          <h1 className="font-serif font-bold text-xl">Top restaurant chains in Nagpur</h1>
           <div>
             <button onClick={Prev}>
               <GoArrowLeft className="h-8 w-8 bg-slate-200 rounded-full p-2 inline mr-2" />
@@ -56,57 +48,62 @@ const TopRestorant = () => {
             </button>
           </div>
         </div>
-          <div className=" flex gap-10">
-        {restaurantInfo.map((restaurant, index) => {
-          const {
-            cloudinaryImageId,
-            aggregatedDiscountInfoV3: discount,
-            name,
-            avgRating,
-            cuisines,
-            sla,
-          } = restaurant.info;
+        <div className="flex gap-10">
+          {restaurantInfo.map((restaurant, index) => {
+            const {
+              cloudinaryImageId,
+              aggregatedDiscountInfoV3: discount,
+              name,
+              avgRating,
+              cuisines,
+              sla,
+            } = restaurant.info;
 
-          const header = discount?.header || "";
-          const subHeader = discount?.subHeader || "";
-          const slaString = sla?.slaString || "";
-          const cuisineList = cuisines?.join(", ") || "N/A";
+            const header = discount?.header || "";
+            const subHeader = discount?.subHeader || "";
+            const slaString = sla?.slaString || "";
+            const cuisineList = cuisines?.join(", ") || "N/A";
 
-          return (
-            <div key={index} 
-            style={{ transform: `translateX(-${Slideritem * 200}%)` }}
-            >
-              <div className="relative my-2">
-                <span className="font-sans absolute left-4 text-xl text-white tracking-tighter bottom-1 font-bold">
-                  {header} {subHeader}
-                </span>
-                <div className="w-[273px] h-[182px]">
-                  <img
-                    className="w-full h-[182px] rounded-xl cursor-pointer object-cover"
-                    src={IMG_URL + cloudinaryImageId}
-                    alt="card-img"
-                  />
-                </div>
-              </div>
-
-              <div className="">
-                <div className=" font-bold text-lg from-neutral-800">
-                  <Link to={"/restaurants/" + restaurant.info.id}>{name}</Link>
-                </div>
-                <div className="flex gap-1 justify-start items-center">
-                  <div className="h-5 w-5 bg-green-700 rounded-full flex items-center justify-center">
-                    <IoMdStarOutline className="h-5 w-5 text-white" />
+            return (
+              <Link to={"/restaurants/" + restaurant.info.id}>
+              <div
+                key={index}
+                className="my-4"
+                style={{ transform: `translateX(-${Slideritem * 200}%)` }}
+              >
+                <div className="relative my-2">
+                  <span className="font-sans absolute left-4 text-xl text-white tracking-tighter bottom-1 font-bold">
+                    {header} {subHeader}
+                  </span>
+                  <div className="w-[273px] h-[182px]">
+                  
+                      <img
+                        className="w-full h-[182px] rounded-xl cursor-pointer object-cover"
+                        src={IMG_URL + cloudinaryImageId}
+                        alt="card-img"
+                      />
+                  
                   </div>
-                  <span className="font-bold">{avgRating}</span>
-                  <span className="font-bold">{slaString}</span>
                 </div>
-                <div className="w-full h-7 overflow-hidden font-light text-lg font-serif from-stone-400">
-                  <p>{cuisineList}</p>
+                <div className="">
+                  <div className="font-bold text-lg from-neutral-800">
+                    <Link to={"/restaurants/" + restaurant.info.id}>{name}</Link>
+                  </div>
+                  <div className="flex gap-1 justify-start items-center">
+                    <div className="h-5 w-5 bg-green-700 rounded-full flex items-center justify-center">
+                      <IoMdStarOutline className="h-5 w-5 text-white" />
+                    </div>
+                    <span className="font-bold">{avgRating}</span>
+                    <span className="font-bold">{slaString}</span>
+                  </div>
+                  <div className="w-full h-7 overflow-hidden font-light text-lg font-serif from-stone-400">
+                    <p>{cuisineList}</p>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
+              </Link>
+            );
+          })}
         </div>
       </div>
     </>
